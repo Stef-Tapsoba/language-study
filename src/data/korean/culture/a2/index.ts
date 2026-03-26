9104bab057268d433e532ef1484e2955921c5c8b
Stef Tapsoba <set89@drexel.edu>
2026-03-26 15:51:20 -0400

    refactor(ja,ko): move A2 culture episodes into a2/ subfolders
    Split flat culture/a2.ts files for Japanese and Korean into individual
    episode files (ja-c-a2-1..3, ko-c-a2-1..3) with an index.ts, matching
    the pattern used by Italian, French, and Spanish.

---
+++ src/data/korean/culture/a2/index.ts
@@ -0,0 +1,6 @@
+import { CultureEpisode } from "../../../../types"
+import { koCA21 } from "./ko-c-a2-1"
+import { koCA22 } from "./ko-c-a2-2"
+import { koCA23 } from "./ko-c-a2-3"
+
+export const koA2Culture: CultureEpisode[] = [koCA21, koCA22, koCA23]
